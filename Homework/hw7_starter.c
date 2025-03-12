adacfbde5f2404d6759187e343f1e4cc8feb8bde
gavinchou94 <gaz11@pitt.edu>
2025-03-12 11:30:06 -0400

    modify HW7 starter code to allow grading of individual parts

--- Homework/hw7_starter.c
+++ Homework/hw7_starter.c
@@ -3,21 +3,55 @@
 
 int main()
 {
-	// 1. Displays integrity statement
+	char pt;
+	printf("Which part of HW7 do you want to test?\n"); // For grading purposes to allow partial credit
+	scanf(" %c", &pt);																	// just put your code of each part inside each case
 
-	// 2. Add your name using comment
+	switch (pt)
+	{
+	case '1':
+	{
+		// Part 1 code here
+		// 1. Displays integrity statement
+		// 2. Add your name using comment
 
-	// 3. Displays the given paragraphs
+		break;
+	}
+	case '3':
+	{
+		// Part 3 code here
+		// 3. Displays the given paragraphs
 
-	// 4. Declare A, B, C. Use scanf to get user input, display accordingly
-	// (for double, use %lf) Display format without space: "<RequiredText><newline>A=?<newline>B=?<newline>C=?<newline>"
+		break;
+	}
+	case '4':
+	{
+		// Part 4 code here
+		// 4. Declare A, B, C. Use scanf to get user input, display accordingly
+		// (for double, use %lf) Display format without space: "<RequiredText><newline>A=?<newline>B=?<newline>C=?<newline>"
 
-	// 5. Declare the G array, and print it out one element at a time. (no need to use a loop)
-	// Display format without space: "G=<newline>value1<newline>value2<newline>value3<newline>value4<newline>"
+		break;
+	}
+	case '5':
+	{
+		// Part 5 code here
+		// 5. Declare the G array, and print it out one element at a time. (no need to use a loop)
+		// Display format without space: "G=<newline>value1<newline>value2<newline>value3<newline>value4<newline>"
 
-	// 6. Declare the H array, and print it out one element at a time. (no need to use a loop)
-	// (for double, use %lf) Format: "H=<newline>value1<newline>value2<newline>value3<newline>value4<newline>"
+		break;
+	}
+	case '6':
+	{
+		// Part 6 code here
+		// 6. Declare the H array, and print it out one element at a time. (no need to use a loop)
+		// (for double, use %lf) Format: "H=<newline>value1<newline>value2<newline>value3<newline>value4<newline>"
 
+		break;
+	}
+	default:
+		printf("Invalid part selected.\n");
+		return 1; // Exit with error
+	}
 	return 0;
 }
 
